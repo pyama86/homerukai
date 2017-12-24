9bab34ae67d0f4154aa2284ddcbe0e03c7c624e8
pyama86 <pyama@pepabo.com>
2017-12-24 18:32:14 +0900

    add 褒め言葉

--- index.js
+++ index.js
@@ -17,13 +17,22 @@ var handlers = {
         this.emit('HomeIntent');
     },
     'HomeIntent': function () {
-      var twitterHelper = new TwitterHelper(this.context.user.accessToken);
+      var twitterHelper = new TwitterHelper(this.event.session.user.accessToken);
 
       var home = [
         '後藤さんは最近良くやってる',
         '最近の後藤さんは実にやっていきがありますね',
         'あの時の後藤さんは、今の僕でも超えるのは難しいでしょうね',
         '後藤さんが後藤さんである所以わかります？',
+        '後藤さんかっこいい！',
+        '後藤さんおしゃれ',
+        '後藤さん、頼りがいがあるよね！',
+        '後藤さん、面白い！楽しい！',
+        '後藤さん、頭の回転が速いよね',
+        '後藤さん、さすがですね！',
+        '後藤さん、いい声してるよね',
+        '後藤さん、優しい、心が広い',
+        '後藤さん、尊敬する',
       ];
 
       twitterHelper.postTweet(home[Math.floor(Math.random() * home.length)])
